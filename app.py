b337c326cca454943c00a6a4ac4d624593fadf7c
sunghwan <psung9510@gmail.com>
2023-02-11 23:03:22 +0900

    Make API, JS File

--- app.py
+++ app.py
@@ -4,12 +4,44 @@ from pymongo import MongoClient
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
-db = client.콜렉션이름
+db = client.mydata
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/post", methods=["POST"])
+def post_article():
+    receive_url = request.form["give_url"]
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    article_url = requests.get(receive_url, headers=headers)
+    soup = BeautifulSoup(article_url.text, "html.parser")
+
+    og_title = soup.select_one("meta[property='og:title']")["content"]
+    og_image = soup.select_one("meta[property='og:image']")["content"]
+    og_url = soup.select_one("meta[property='og:url']")["content"]
+    og_desc = soup.select_one("meta[property='og:description']")["content"]
+
+    doc = {
+        "title": og_title,
+        "image": og_image,
+        "url": og_url,
+        "desc": og_desc
+    }
+
+    db.articles.insert_one(doc)
+
+    return jsonify({"result": "success"})
+
+
+@app.route("/read", methods=["GET"])
+def show_articles():
+    data = list(db.articles.find({}, {"_id": False}))
+    return jsonify({"result": "success", "info": data})
 
 
 if __name__ == "__main__":
---
+++ static/index.js
@@ -0,0 +1,61 @@
+$(document).ready(() => showArticles());
+
+const openClose = function () {
+  const status = $("#url-box").css("display");
+  if (status == "block") {
+    $("#openClose-btn").text("URL박스 열기");
+    $("#url-box").hide();
+  } else {
+    $("#openClose-btn").text("URL박스 닫기");
+    $("#url-box").show();
+  }
+};
+
+const posting = function () {
+  const articleURL = $("#posting-url").val();
+  $.ajax({
+    type: "POST",
+    url: "/post",
+    data: { give_url: articleURL },
+    success: function (response) {
+      if (response["result"] == "success") {
+        alert("포스팅 성공");
+        window.location.reload();
+      }
+    },
+  });
+};
+
+const showArticles = function () {
+  $.ajax({
+    type: "GET",
+    url: "/read",
+    data: {},
+    success: function (response) {
+      const articles = response["info"];
+      for (let i = 0; i < articles.length; i++) {
+        makeCard(
+          articles[i]["title"],
+          articles[i]["image"],
+          articles[i]["url"],
+          articles[i]["desc"]
+        );
+      }
+    },
+  });
+};
+
+const makeCard = function (title, img, url, desc) {
+  const templateHtml = `<div class="col mb-4">
+                            <div class="card h-100">
+                                <img src="${img}" class="card-img-top" alt="image-cap" />
+                                <div class="card-body">
+                                    <h5 class="card-title"><a href="${url}">${title}</a></h5>
+                                    <p class="card-text">
+                                    ${desc}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>`;
+  $("#card-box").append(templateHtml);
+};
